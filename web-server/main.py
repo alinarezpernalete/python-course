38184faff5d6ffa002b54190ae5eaee3366fe093
Angel Linarez Pernalete <alinarezpernalete@gmail.com>
2024-06-22 16:54:57 -0400

    web server con fast api

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+	return [1,2,3,4,]
+
+@app.get('/contact')
+def get_list():
+	return {"name": "Platzi"}
+
+@app.get('/page', response_class=HTMLResponse)
+def get_page():
+	return """
+		<h1>Pagina desde Python</h1>
+		<p>Parrafo</p>
+	"""
 
 def run():
 	store.get_categories()
